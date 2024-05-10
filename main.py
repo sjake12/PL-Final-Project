a3de595aaa6cc322392a6623cdc7b666f3e1fd1a
sjake12 <apostol.sjake12@gmail.com>
2024-05-10 08:44:27 +0800

    Yt-EP1-final

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pl
 
 while True:
     text = input('pl > ')
-    result, error = basic.run(text)
+    result, error = pl.run('<stdin>'.text)
 
     if error: print(error.as_string())
     else: print(result)
--- pl.py
+++ pl.py
@@ -5,17 +5,43 @@ DIGITS = '0123456789'
 ###############ERRORS
 
 class Error:
-    def __init__(self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
 
     def as_string(self):
         result = f'{self.error_name}: {self.details}'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
 
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
+    def __init__(self, pos_start, pos_end, details,):
+        super().__init__(post_start, pos_end, 'Illegal Character', details)
+
+##################POSITION
+
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+            
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col,self.fn, self.ftxt)
 
 ########TOKENS
 
@@ -23,6 +49,7 @@ TT_INT = 'TT_INT'
 TT_FLOAT = 'FLOAT'
 TT_PLUS = 'PLUS'
 TT_MINUS = 'MINUS'
+TT_MUL = 'MUL'
 TT_DIV = 'DIV'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
@@ -39,15 +66,16 @@ class Token:
 ###########LEXER
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
 
     def advance(self):
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance()
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     def make_tokens(self):
         tokens = []
@@ -76,9 +104,10 @@ class Lexer:
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
             else:
+                pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError("'" + char + "'")
+                return [], IllegalCharError(pos_start, self.pos,"'" + char + "'")
 
         return tokens, None
 
@@ -92,7 +121,7 @@ class Lexer:
                 dot_count += 1
                 num_str += '.'
             else:
-                num_stf += self.current_char
+                num_str += self.current_char
             self.advance()
 
         if dot_count == 0:
@@ -102,8 +131,8 @@ class Lexer:
 
 ##################RUN
 
-def run(text):
-    Lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
 
     return tokens, error
